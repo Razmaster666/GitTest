733212561e1412001519d7abde909ea53d170361
RasmusClausen <rasmusclausen123@gmail.com>
2022-11-08 16:14:18 +0100

    I commited a sick system out

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! Change 4");
+        System.out.println("Hello world, again! Change 5");
     }
 }
